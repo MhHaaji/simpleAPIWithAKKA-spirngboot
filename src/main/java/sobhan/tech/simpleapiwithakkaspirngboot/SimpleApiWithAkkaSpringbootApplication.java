a1b2ad8bacd45d11446cf2bbbda887065ead1898
MHHaaji <soldier.mh.2001@gmail.com>
2021-11-24 17:00:58 +0330

    some refactor in names

--- src/main/java/sobhan/tech/simpleapiwithakkaspirngboot/SimpleApiWithAkkaSpringbootApplication.java
+++ src/main/java/sobhan/tech/simpleapiwithakkaspirngboot/SimpleApiWithAkkaSpringbootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleApiWithAkkaSpirngbootApplication {
+public class SimpleApiWithAkkaSpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SimpleApiWithAkkaSpirngbootApplication.class, args);
+        SpringApplication.run(SimpleApiWithAkkaSpringbootApplication.class, args);
     }
 
 }
